ffa5c1c9b90f5175ae66d4e1919684f8629b281c
Jay Bergonia <72058376+fjbMultisys@users.noreply.github.com>
2021-09-01 13:50:33 +0800

    rich app 2021

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point for all our Flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text('Northern Lights'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
